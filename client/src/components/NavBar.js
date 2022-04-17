5910b6109759f96b44e3bc9a775dad7abef52089
Vladislav Kulyakin <kulyakinv@gmail.com>
2022-04-17 17:41:20 +0300

    add base layout authorization page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,7 +3,7 @@ import { Context } from '..'
 import { observer } from 'mobx-react-lite'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import {Button} from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 import Container from 'react-bootstrap/Container'
 import { NavLink } from 'react-router-dom'
 import { SHOP_ROUTE } from '../utils/constatns'
@@ -14,35 +14,35 @@ const NavBar = observer(() => {
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
-            <NavLink
-                className="ml-auto"
-                style={{ color: 'white' }}
-                to={SHOP_ROUTE}
-            >
-                Cloth-Store
-            </NavLink>
-            {user.isAuth ? 
-                <Nav className="ml-auto">
-                    <Button  variant={'outline-light'}
-                        className='mx-3'
-                        >Admin-panel</Button>
-                    <Button
-                        variant={'outline-light'}
-                        onClick={() => user.setIsAuth(false)}
-                    >
-                        Log out
-                    </Button>
-                </Nav>
-             : 
-                <Nav className="ml-auto">
-                    <Button
-                        variant={'outline-light'}
-                        onClick={() => user.setIsAuth(true)}
-                    >
-                        Log in
-                    </Button>
-                </Nav>
-            }
+                <NavLink
+                    className="ml-auto"
+                    style={{ color: 'white' }}
+                    to={SHOP_ROUTE}
+                >
+                    Cloth-Store
+                </NavLink>
+                {user.isAuth ? (
+                    <Nav className="ml-auto">
+                        <Button variant={'outline-light'} className="mx-3">
+                            Admin-panel
+                        </Button>
+                        <Button
+                            variant={'outline-light'}
+                            onClick={() => user.setIsAuth(false)}
+                        >
+                            Log out
+                        </Button>
+                    </Nav>
+                ) : (
+                    <Nav className="ml-auto">
+                        <Button
+                            variant={'outline-light'}
+                            onClick={() => user.setIsAuth(true)}
+                        >
+                            Log in
+                        </Button>
+                    </Nav>
+                )}
             </Container>
         </Navbar>
     )
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,7 +1,51 @@
 import React from 'react'
+import { Button, Card, Container, Form } from 'react-bootstrap'
+import { NavLink, useLocation } from 'react-router-dom'
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/constatns'
 
 const Auth = () => {
-    return <div>auth</div>
+    const location = useLocation()
+    const isLogin = location.pathname === LOGIN_ROUTE
+    console.log(location)
+
+    return (
+        <Container
+            className="d-flex justify-content-center align-items-center"
+            style={{ height: window.innerHeight - 54 }}
+        >
+            <Card style={{ width: 600 }} className="p-5">
+                <h2 className="m-auto">{isLogin ? 'Log In' : 'Sign Up'}</h2>
+                <Form className="d-flex flex-column">
+                    <Form.Control
+                        className="mt-4"
+                        placeholder="E-mail"
+                    ></Form.Control>
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Password"
+                    ></Form.Control>
+                    <div className="d-flex flex-row justify-content-between mt-3 pl-3 pr-3">
+                        {isLogin ? (
+                            <div>
+                                Don't have an account?{' '}
+                                <NavLink to={REGISTRATION_ROUTE}>
+                                    Sign Up
+                                </NavLink>
+                            </div>
+                        ) : (
+                            <div>
+                                Do you have an account?{' '}
+                                <NavLink to={LOGIN_ROUTE}>Sign In</NavLink>
+                            </div>
+                        )}
+                        <Button variant="outline-dark">
+                            {isLogin ? 'Log in' : 'Sign up'}
+                        </Button>
+                    </div>
+                </Form>
+            </Card>
+        </Container>
+    )
 }
 
 export default Auth
